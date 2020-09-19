046a7d69244ce54d0cc0d253c4e6d142d31397a6
ssevenseass <k-kuksenko@mail.ru>
2020-09-19 07:13:08 +0300

    new commit

--- lab1.sh
+++ lab1.sh
@@ -1,71 +1,65 @@
-#2.2
 #!/bin/bash
 
-if [[ $1 == "calc" ]]; then
-   read number1 char number2
-case $char in
+comand="$1"
+  case $comand in
+
+#task_2.2
+#!/bin/bash
+
+"calc")
+  if [ -z "$2" ];  then
+read number1 char number2
+  case $char in
 "+") echo $((number1 + number2));;
 "-") echo $((number1 - number2));;
 "*") echo $((number1 * number2));;
-"/") [ $number2 -eq 0 ] && echo "error" || echo $((number1 / number2));;
-esac
-  else echo "error"
-fi
-
-
-if [[ $1 == "calc" ]] && [[ $2 == "sum" ]]; then
-  read number1 char number2
-case $char in
-  "+") echo $((number1 + number 2));;
-esac
-  else echo "error"
-fi
-
-if [[ $1 == "calc" ]] && [[ $2 == "sub" ]]; then
-  read number1 char number2
-case $char in
-  "-") echo $((number1 - number2));;
-esac
-  else echo "error"
-fi
-
-if [[ $1 == "calc" ]] && [[ $2 == "mul" ]]; then
-  read number1 char number2
-case $char in
-  "*") echo $((number1 * number2));;
-esac
-  else echo "error"
-fi
-
-if [[ $1 == "calc" ]] && [[ $2 == "div" ]]; then
-  read number1 char number2
-case $char in
-  "/") echo $((number1 / number2));;
-esac
-  else echo "error"
-fi
-
-#2.3
-#!/bin/bash
-
-if [[ $1 == "search"]] ; then
-  DIR="$2"
-if [ -d "$DIR" ]; then
-  ls "$DIR" | grep -r "$3"
+"/") [ $number2 -eq 0] && echo "error" || echo $((number1 / number2));;
+  esac
 else
-  echo "directory not found"
-fi
+  read number1 number2
+argument="$2"
+  case $argument in
+"sum") echo $((number1 + number2));;
+"sub") echo $((number1 - number2));;
+"mul") echo $((number1 * number2));;
+"div") [ $number2 -eq 0 ] && echo "error" || echo $((number1 / number2));;
+  esac
+fi;;
 
-#2.4
+#task_2.3
 #!/bin/bash
 
-if [[ $1 == "reverse" ]]; then
-  firstfile="$2"
-  secondfile="$3"
-if [ -f $firstfile ]; then
-  sort $firstfile -r  > $secondfile
-fi
+"search")
+  DIR="$2"
+if [ -d "$DIR" ];  then
+  ls "$DIR" | grep "$3"
 else
   echo "error"
-fi
+fi;;
 
+#task_2.4
+#!/bin/bash
+
+"reverse")
+  firstfile="$2"
+  secondfile="$3"
+if [ -f $firstfile ];  then
+  sort $firstfile -r > $secondfile
+else
+  echo "error"
+fi;;
+
+#task_2.5
+#!/bin/bash
+
+"strlen")
+if [ -z "$2" ];  then
+  echo "error"
+else
+  str="$2"
+  echo ${#str}
+fi;;
+
+*)
+  echo "error"
+esac
