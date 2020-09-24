6b5ff72251a2f3d4ae20e2dfd6b3f4ba39c430b5
ssevenseass <k-kuksenko@mail.ru>
2020-09-24 13:51:20 +0300

    new commit

--- reverse.sh
+++ reverse.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 function reverse() {
-  firstfile="$2"
-  secondfile="$3"
+  firstfile="$1"
+  secondfile="$2"
  if [ -f $firstfile ]; then
   sort $firstfile -r > $secondfile
 else echo "error" > /dev/stderr
