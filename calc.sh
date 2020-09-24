ab986e6d543316a02bab228e5fcc8d11a06da541
ssevenseass <k-kuksenko@mail.ru>
2020-09-24 13:22:55 +0300

    new commit

---
+++ calc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function calc() {
+ if [ -z "$2" ]; then
+read number1 char number2
+  case $char in
+"+") echo $((number1 + number2));;
+"-") echo $((number1 - number2));;
+"*") echo $((number1 * number2));;
+"/") [ $number2 -eq 0 ] && echo "error" || echo $((number1 / number2));;
+ esac
+else
+
+read number1 number2
+ argument="$2"
+  case $argument in
+"sum") echo $((number1 + number2));;
+"sub") echo $((number1 - number2));;
+"mul") echo $((number1 * number2));;
+"div") [ $number2 -eq 0 ] && echo "error" || echo $((number / number2));;
+ esac
+fi
+}
+
+
