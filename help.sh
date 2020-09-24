435ceaf25587a9c86ff46cb400beeac58af11cf3
ssevenseass <k-kuksenko@mail.ru>
2020-09-25 01:19:24 +0300

    new commit

--- help.sh
+++ help.sh
@@ -4,10 +4,18 @@
 echo "APPLICATION HELP.
  A. The command CALC - number1, char, number2, sum, sub, mul, div.
  B. The command SEARCH - directory.
- C. The command RESERVE - firstfile, secondfile.
+ C. The command REVERSE - firstfile, secondfile.
  D. The command STRLEN - string.
  E. The command LOG.
  F. The command EXIT.
- G. The command HELP."
+ G. The command HELP.
+ Return code:
+ exit 11 -
+ exit 33 -
+ exit 22 -
+ exit 77 -
+ exit 55 -
+ exit 66 -
+ exit 12 -  "
 
 
