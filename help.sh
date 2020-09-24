e7196e5e9540a2c4c7a7ea7261fc83cac6e5734c
ssevenseass <k-kuksenko@mail.ru>
2020-09-24 18:03:25 +0300

    new commit

--- help.sh
+++ help.sh
@@ -2,7 +2,14 @@
 
 function help() {
 
-echo "APPLICATION HELP....."
-
+echo "APPLICATION HELP.
+ A. The command CALC - number1, char, number2, sum, sub, mul, div.
+ B. The command SEARCH - directory.
+ C. The command RESERVE - firstfile, secondfile.
+ D. The command STRLEN - string.
+ E. The command LOG.
+ F. The command EXIT.
+ G. The command HELP."
 }
+
 
