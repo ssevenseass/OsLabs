2ed30ae94f26466a210fb711d8f3ac01821d00ef
ssevenseass <k-kuksenko@mail.ru>
2020-09-24 14:17:48 +0300

    new commit

--- log.sh
+++ log.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+function log() {
+
+  information="\\(II\\)"
+  warning="\\(WW\\)"
+ grep -E -h -s $information /var/log/anaconda/X.log | sed -E "s/$information/Information: /" >> filename.log
+ grep -E -h -s $warning /var/log/anaconda/X.log | sed -E "s/$warning/Warning: /" >> filename.log
+}
+
