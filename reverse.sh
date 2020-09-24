802ba66bf6f06775019eeb6db55955442bfb7c6e
ssevenseass <k-kuksenko@mail.ru>
2020-09-25 01:08:50 +0300

    new commit

--- reverse.sh
+++ reverse.sh
@@ -7,3 +7,15 @@
   sort $firstfile -r > $secondfile
 else echo "error"
 fi
+  if [[ -w "$2" ]]; then
+echo "error" > /dev/stderr
+  bash help.sh
+echo exit 55;
+fi
+
+  if [[ $# -ne 2 ]]; then
+echo "error" > /dev/stderr
+  bash help.sh
+echo exit 66;
+fi
+
