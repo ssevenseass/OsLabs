2279ce513efbd9f060196cb9fd752459f9e7cb12
ssevenseass <k-kuksenko@mail.ru>
2020-09-25 01:05:20 +0300

    new commit

--- search.sh
+++ search.sh
@@ -4,5 +4,12 @@
   if [ DIR="$1" ] && [ -d "$DIR" ]; then
  ls "$DIR" | grep "$2"
 else echo "error" > /dev/stderr
+ bash help.sh
+echo exit 22;
 fi
 
+if [[ $# -ne 2 ]]; then
+  echo "error" > /dev/stderr
+bash help.sh
+echo exit 77;
+fi
