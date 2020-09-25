ca40f446ac16ed5788489a3cda3dee58fe712296
ssevenseass <k-kuksenko@mail.ru>
2020-09-25 06:22:06 +0300

    new

--- help.sh
+++ help.sh
@@ -10,14 +10,15 @@ echo "APPLICATION HELP.
  F. The command EXIT.
  G. The command HELP.
  Return code:
- exit 11 -
- exit 33 -
- exit 22 -
- exit 77 -
- exit 55 -
- exit 66 -
- exit 12 -
- exit 88 -
- exit 1 -    "
+ exit 11 - the number of arguments passed to the script is not equal to 3.
+ exit 77 - the number of arguments passed to the script is not equal 2.
+ exit 22 - directory is not the first argument and does not exist.
+ exit 21 - no arguments.
+ exit 12 - argument not found.
+ exit 88 - first argument is not equal calc/search/reverse/strlen/log/exit/help/interactive.
+ exit 1 - file not found.
+ exit 18 - is not a number.
+ exit 20 - no directory.
+ exit 19 - file does not exist."
 
 
