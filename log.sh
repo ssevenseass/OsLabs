38b89070429066a9f849132472bda77e55cf6dae
ssevenseass <k-kuksenko@mail.ru>
2020-09-25 07:55:01 +0300

    new

--- log.sh
+++ log.sh
@@ -7,12 +7,12 @@
   echo exit 1;
  else
 
- sed -n "s/] (WW)/] Warning:/p" $name > filename.txt
- sed -n "s/] (II)/] Information:/p" $name > filename.txt
+ sed "s/] (WW)/] Warning:/p" $name > filename
+ sed "s/] (II)/] Information:/p" $name > filename
 
-GREP_COLOR="1;33" grep --color="always" "Warning" filename.txt
-GREP_COLOR="1;34" grep --color="always" "Information" filename.txt
- rm filename.txt
+GREP_COLOR="1;33" grep --color="always" "Warning" filename
+GREP_COLOR="1;34" grep --color="always" "Information" filename
 
+ cat filename
 fi
 
