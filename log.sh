d3ad7063b845e3f3ede7e589ec9566d91485e823
ssevenseass <k-kuksenko@mail.ru>
2020-09-25 07:14:37 +0300

    new

--- log.sh
+++ log.sh
@@ -7,13 +7,12 @@
   echo exit 1;
  else
 
-  info="\\(II\\)"
-  warn="\\(WW\\)"
- grep -E -h -s $warn  $name  | sed -E "s/$warn/Warning: /" > filename.txt
- grep -E -h -s $info $name | sed -E "s/$warn/Information: /" > filename.txt
+ sed -n "s/] (WW)/] Warning:/p" $name > filename.txt
+ sed -n "s/] (II)/] Information:/p" $name > filename.txt
 
 GREP_COLOR="1;33" grep --color="always" "Warning" filename.txt
 GREP_COLOR="1;34" grep --color="always" "Information" filename.txt
  rm filename.txt
 
 fi
+
