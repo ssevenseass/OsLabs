93a827df03b0e4ffeb92400496db150a9aa39010
ssevenseass <k-kuksenko@mail.ru>
2020-09-25 09:52:32 +0300

    new

--- inter.sh
+++ inter.sh
@@ -19,10 +19,12 @@ function My_menu {
 My_menu
 printf "Enter the section: "
  read -r option
-printf "Enter args: "
- read -r args
   case $option in
 A|calc)
+
+printf "Enter args: "
+ read -r args
+
  echo "Enter: value1"
 read -r val1
  echo "Enter value2"
@@ -30,14 +32,27 @@ read -r val2
 
  bash calc.sh "$args" "$val1" "$val2";;
 B|search)
+
+printf "Enter args: "
+ read -r args
   bash search.sh "$args";;
 C|reverse)
-  bash reverse.sh "$args";;
+echo "Enter 1 arg:"
+read -r arg1
+echo "Enter 2 arg"
+read -r arg2
+  bash reverse.sh "$args" "$arg1" "$arg2";;
 D|strlen)
+
+printf "Enter args: "
+ read -r args
   bash strlen.sh "$args";;
 E|log)
   bash log.sh "$args";;
 F|exit)
+
+printf "Enter args: "
+ read -r args
   bash exit.sh "$args";;
 G|quit)
   break 2;;
