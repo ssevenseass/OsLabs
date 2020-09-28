78197d5ea9a34c54253004b77d03c7ab42e852a5
ssevenseass <k-kuksenko@mail.ru>
2020-09-28 04:43:54 +0300

    new

--- help.sh
+++ help.sh
@@ -8,17 +8,7 @@ echo "APPLICATION HELP.
  D. The command STRLEN - string.
  E. The command LOG.
  F. The command EXIT.
- G. The command HELP.
- Return code:
- exit 11 - the number of arguments passed to the script is not equal to 3.
- exit 77 - the number of arguments passed to the script is not equal 2.
- exit 22 - directory is not the first argument and does not exist.
- exit 21 - no arguments.
- exit 12 - argument not found.
- exit 88 - first argument is not equal calc/search/reverse/strlen/log/exit/help/interactive.
- exit 1 - file not found.
- exit 18 - is not a number.
- exit 20 - no directory.
- exit 19 - file does not exist."
+ G. The command HELP."
+
 
 
